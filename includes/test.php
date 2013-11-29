02c6d5e19f137dc4d7effe0dafd6f6b24d4b37ef
sharmel <adedoyin.israel@gmail.com>
2013-11-29 22:17:39 +0200

    Update test.php

--- includes/test.php
+++ includes/test.php
@@ -1,6 +1,6 @@
 <?php
 // If it's going to need the database, then it's 
-// probably smart to require it before we start.
+// probably smart to require it before starting.
 require_once(LIB_PATH.DS.'database.php');
 
 class User extends DatabaseObject {
